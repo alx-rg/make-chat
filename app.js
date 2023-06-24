80006d2c188cb7ecf84778b42565a808174ac9c6
Alex Ross <alxrg@me.com>
2023-06-24 16:27:33 -0500

    Users can log in

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
 const exphbs  = require('express-handlebars');
-
 //Socket.io
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
   console.log("🔌 New user connected! 🔌");
+  require('./sockets/chat.js')(io, socket);
 })
 
-app.engine('handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "index"}));
+app.engine('handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "index",
+layoutsDir: "views"}));
 app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
 })
-
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
 })
--- public/index.js
+++ public/index.js
@@ -1,4 +1,21 @@
-$(document).ready( () => {
-  //Connect to the socket.io server
+// index.js
+$(document).ready(()=>{
   const socket = io.connect();
+
+  $('#create-user-btn').click((e)=>{
+    e.preventDefault();
+    let username = $('#username-input').val();
+    console.log(username)
+    if(username.length > 0){
+      //Emit to the server the new user
+      socket.emit('new user', username);
+      $('.username-form').remove();
+    }
+  });
+
+
+  socket.on('new user', (username) => {
+    console.log(`✋ ${username} has joined the chat! ✋`);
+  })
+
 })
